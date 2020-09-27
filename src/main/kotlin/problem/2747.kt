3617122a741cd9be8d782614b0b6df474595a2e4
hyecheon <rainbow880616@gmail.com>
2020-09-27 19:32:48 +0900

    https://www.acmicpc.net/problem/2747

--- src/main/kotlin/problem/2747.kt
+++ src/main/kotlin/problem/2747.kt
@@ -1,27 +1,10 @@
 package problem
 
 fun main() {
-    val br = System.`in`.bufferedReader()
-    println(fibo(br.readLine().toInt()))
+    println(fibo(System.`in`.bufferedReader().readLine().toInt()))
 }
 
 fun fibo(n: Int): Long {
-    fun go(a: Long, b: Long, n: Int): Long {
-        return if (n == 0) {
-            a
-        } else {
-            go(b, a + b, n - 1)
-        }
-    }
-    return when (n) {
-        0 -> {
-            0
-        }
-        1 -> {
-            1
-        }
-        else -> {
-            go(0L, 1L, n)
-        }
-    }
+    tailrec fun go(a: Long, b: Long, n: Int): Long = if (n == 0) a else go(b, a + b, n - 1)
+    return go(0L, 1L, n)
 }
