2db849b00590223c8ac177da866c91d6349ac90a
이혜천 <rainbow880616@gmail.com>
2021-04-29 23:53:14 +0900

    취미생활중 링크드리스트 삭제.

--- src/main/kotlin/datastructure/MyLinkedList.kt
+++ src/main/kotlin/datastructure/MyLinkedList.kt
@@ -18,7 +18,26 @@ class MyLinkedList {
         }
     }
 
-    fun nodePrint() {
+    fun del(data: Int) {
+        if (head == null) return
+        if (data == head!!.data) {
+            head = head!!.next
+            return
+        }
+        var node = head!!
+        while (node.next != null) {
+            if (node.next!!.data == data) {
+                if (node.next == last) {
+                    last = node
+                }
+                node.next = node.next!!.next
+                break
+            }
+            node = node.next!!
+        }
+    }
+
+    fun desc() {
         var node = head
         while (node != null) {
             print("${node.data}, ")
@@ -37,5 +56,18 @@ fun main() {
     for (data in 1..10) {
         myLinkedList.add(data)
     }
-    myLinkedList.nodePrint()
+    myLinkedList.desc()
+    println()
+    myLinkedList.del(10)
+    myLinkedList.desc()
+    println()
+    myLinkedList.add(11)
+    myLinkedList.desc()
+    println()
+    myLinkedList.del(1)
+    myLinkedList.desc()
+    println()
+    myLinkedList.del(5)
+    myLinkedList.desc()
+    println()
 }
--- src/main/kotlin/datastructure/MyStack.kt
+++ src/main/kotlin/datastructure/MyStack.kt
@@ -1,4 +1,4 @@
-package datastructure
+package datastructure.MyStack
 
 /**
  * User: hyecheon lee
